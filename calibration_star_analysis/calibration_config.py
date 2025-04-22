1a5e1a696df3827677549c9a49a106bb1405f164
elenacarlson <elenacarlson1@icloud.com>
2025-04-22 17:30:45 -0600

    Update calibration_config.py
    remove excess constants

--- calibration_star_analysis/calibration_config.py
+++ calibration_star_analysis/calibration_config.py
@@ -3,10 +3,12 @@
 ###############
 
 # path to files
-file_path = "/Users/elenacarlson/PycharmProjects/Calibration_Stars/data_files"
+file_path = "/home/elca1097/LASP_SPRITE/data_files/"
 
 # path to save figures
-figure_path = "/Users/elenacarlson/PycharmProjects/Calibration_Stars/figures/"
+figure_path = "/home/elca1097/LASP_SPRITE/code/split_up_code/calibration/demo_figures/"
+
+star_data_path = "/home/elca1097/LASP_SPRITE/standard_star_data/"
 
 ###########################
 # detector/slit constants #
@@ -43,49 +45,21 @@ min_x_ang = slit_loc_ang - ang_before_ap
 max_x_ang = slit_loc_ang + ang_after_ap
 ang_range = max_x_ang - min_x_ang
 
-# slit constants
-x_pix = 40  # slit pixel width
-y_pix = 760  # slit pixel length
-
-##########################
-# SNR and Star Constants #
-##########################
-
-# Point Source standard dev for gaussian
-# 140 arcsecond/mm
-arcsec_per_pix = mm_per_x_pixel * 140
-
-# Invert for pixels per arcsecond
-pix_per_arcsec = 1/arcsec_per_pix
-
-# Multiply by angular size of 14 arcsecond
-pix_std_dev = 14 * pix_per_arcsec
-
-# Supernova Remnant size in arcsec
-SN_width = 1.4 * 60
-SN_height = 1.2 * 60
 
 #########
 # Noise #
 #########
 
 # Dark rate
+
 '''
 in a best case scenario:
 dark_rate = 0.5 * 1e-4**2  #counts/micron^2/s
 '''
+
 dark_rate = 2 * 1e-4**2  # counts/micron^2/s
 
 # noise per angstrom for a point source
 point_source_pix_height = 150  # microns
 angstrom_width = 45  # microns
 noise = point_source_pix_height * angstrom_width * dark_rate  # counts/angstrom/second
-
-###########################
-# Dictionary of Constants #
-###########################
-
-# Constants used for convolution
-convolve_constants = {max_x_ang: (slit_loc_ang + ang_after_ap), min_x_ang: (slit_loc_ang - ang_before_ap),
-                      ang_range: (max_x_ang - min_x_ang), row_pixels: 760, col_pixels: 1380, SN_height: (1.2*60),
-                      pix_std_dev: (14 * pix_per_arcsec)}
